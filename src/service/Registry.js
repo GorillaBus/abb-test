5ceb27b9d1fc4860f482ecee3fd6be65540b4e68
Edu <eduugr@gmail.com>
2020-01-22 22:12:39 +0100

    Created save method on Registry service to save new logs

--- src/service/Registry.js
+++ src/service/Registry.js
@@ -1,27 +1,13 @@
-//const Queue = require("queue");
 'use strict'
 
 const Registry = (appSettings, Logger, mongoose, models) => {
 
-	const test = () => {
-
-		return registerControl({
-			dev_x: 0.02,
-			dev_y: 0.03,
-			dev_z: 0.01,
-			dev_out_x: 0.04,
-			dev_out_y: 0.06,
-			dev_out_z: 0.05,
-			diameter: 1.452
-		});
-	};
-
-	const registerControl = (controlData) => {
-		return models.Control.create(controlData);
+	const save = (report) => {
+		return models.Log.create(report);
 	};
 
 	return {
-		test: test
+		save
 	}
 };
 
