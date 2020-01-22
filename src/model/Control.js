2489f541ebfcb5928958bd2bb74a586cfc80d38d
Edu <eduugr@gmail.com>
2020-01-22 21:56:44 +0100

    Removed spaces

--- src/model/Control.js
+++ src/model/Control.js
@@ -6,17 +6,14 @@ const model = (mongoose) => {
 		machine_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Machine', required: true },
 		feature_id: { type: Number, required: true },
 		feature_title: { type: String, required: true },
-
 		x: { type: Number, required: true },
 		y: { type: Number, required: true },
 		z: { type: Number, required: true },
 		d: { type: Number, required: true },
-
 		x_tol: { type: Number, required: true },
 		y_tol: { type: Number, required: true },
 		z_tol: { type: Number, required: true },
 		d_tol: { type: Number, required: true },
-
 		ord: { type: Number, required: true }
 	});
 
