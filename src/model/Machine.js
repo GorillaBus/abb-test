1e716e1ff4f934d4c7da10ad61b9ad2cf714e446
Edu <eduugr@gmail.com>
2020-01-25 01:24:14 +0100

    Added required fields to Machine model

--- src/model/Machine.js
+++ src/model/Machine.js
@@ -4,7 +4,9 @@ const model = (mongoose) => {
 
 	const MachineSchema = new mongoose.Schema({
 		user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-		description: { type: String, required: false }
+		model: { type: String, required: false },
+		description: { type: String, required: false },
+		online: { type: Boolean, default: false }
 	});
 
 	const model = mongoose.model("Machine", MachineSchema);
